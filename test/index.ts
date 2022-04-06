f9f6c6eb2659e3852f7760fa2db3623bdcf46850
Serhii Makohon <serhii.makohon@neatfusion.org>
2022-04-06 16:45:36 +0300

    BG-7/ create test

--- test/index.ts
+++ test/index.ts
@@ -26,16 +26,13 @@ describe("Bridge", function () {
   it("Should deposit tokens to bridge", async function () {
     const [owner] = await ethers.getSigners();
     const ownerAddress = owner.address;
-    const addressToken = "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF";
+    const validators = [ownerAddress];
     const destinationToken = "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF";
     
     const chainId = 123;
     const mintAmount = "100000000000000000000";
     const depositAmount = "10000000000000000000";
-
-    const Bridge = await ethers.getContractFactory("Bridge");
-    const bridge = await Bridge.deploy();
-    await bridge.deployed();
+    const initialRequiredApprovals = 1;
 
     const MockToken = await ethers.getContractFactory("MockToken");
     const mockToken = await MockToken.deploy("Token", "TKN", mintAmount);
@@ -46,9 +43,12 @@ describe("Bridge", function () {
     await tokenManager.deployed();
 
     await (await tokenManager.initialize(ownerAddress)).wait();
+    await expect(tokenManager.addSupportedToken(chainId, mockToken.address, destinationToken));
 
-    await expect(tokenManager.addSupportedToken(chainId, addressToken, destinationToken)).to.be
-    .revertedWith("only owner"); // ERROR 
+    const Bridge = await ethers.getContractFactory("Bridge");
+    const bridge = await Bridge.deploy();
+    await bridge.deployed();
+    await (await bridge.initialize(ownerAddress, validators, initialRequiredApprovals, tokenManager.address)).wait();
 
     await mockToken.approve(bridge.address, depositAmount);
     await bridge.deposit(mockToken.address, chainId, depositAmount);
@@ -62,15 +62,24 @@ describe("Bridge", function () {
     const depositAmount = "10000000000000000000";
     
     const txHash = "0x54c96e7f79d5fd653951c49783fc2fa7299f14c01a5a3a03f8bfb55eecb2751f";
+    const destinationToken = "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF";
 
     const MockToken = await ethers.getContractFactory("MockToken");
     const mockToken = await MockToken.deploy("Token", "TKN", mintAmount);
     await mockToken.deployed();
 
+
+    const TokenManager = await ethers.getContractFactory("TokenManager");
+    const tokenManager = await TokenManager.deploy();
+    await tokenManager.deployed();
+
+    await (await tokenManager.initialize(owner.address)).wait();
+    await expect(tokenManager.addSupportedToken(chainId, mockToken.address, destinationToken));
+
     const Bridge = await ethers.getContractFactory("Bridge");
     const bridge = await Bridge.deploy();
     await bridge.deployed();
-    await (await bridge.initialize(owner.address, [v1.address, v2.address, v3.address], 2, mockToken.address)).wait();
+    await (await bridge.initialize(owner.address, [v1.address, v2.address, v3.address], 2, tokenManager.address)).wait();
 
     await mockToken.approve(bridge.address, depositAmount);
     await bridge.deposit(mockToken.address, chainId, depositAmount);
@@ -97,26 +106,81 @@ describe("Bridge", function () {
 
     expect(await bridge.executed(id)).to.equal(true);
   });
-});
 
-describe("TokenManager", function () {
-  it("Should add support token", async function () {
+  it("Should deposit supported tokens to bridge", async function () {
     const [owner] = await ethers.getSigners();
     const ownerAddress = owner.address;
-    const addressToken = "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF";
+    const validators = [ownerAddress];
     const destinationToken = "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF";
-
+    
     const chainId = 123;
+    const mintAmount = "100000000000000000000";
+    const depositAmount = "10000000000000000000";
+    const initialRequiredApprovals = 1;
+
+    const MockToken = await ethers.getContractFactory("MockToken");
+    const mockToken = await MockToken.deploy("Token1", "TKN1", mintAmount);
+    await mockToken.deployed();
+
+    const mockToken2 = await MockToken.deploy("Token2", "TKN2", mintAmount);
+    await mockToken2.deployed();
 
     const TokenManager = await ethers.getContractFactory("TokenManager");
     const tokenManager = await TokenManager.deploy();
     await tokenManager.deployed();
 
     await (await tokenManager.initialize(ownerAddress)).wait();
+    await expect(tokenManager.addSupportedToken(chainId, mockToken.address, destinationToken));
 
-    await expect(tokenManager.addSupportedToken(chainId, addressToken, destinationToken)).to.be
-    .revertedWith("only owner"); // ERROR 
+    const Bridge = await ethers.getContractFactory("Bridge");
+    const bridge = await Bridge.deploy();
+    await bridge.deployed();
+    await (await bridge.initialize(ownerAddress, validators, initialRequiredApprovals, tokenManager.address)).wait();
+
+    await mockToken.approve(bridge.address, depositAmount);
+    await mockToken2.approve(bridge.address, depositAmount);
+
+    await bridge.deposit(mockToken.address, chainId, depositAmount);
     
+    await expect( bridge.deposit(mockToken2.address, chainId, depositAmount)).to.be
+    .revertedWith("Token is not supported");
+  });  
+});
+
+describe("TokenManager", function () {
+  it("Should called addSupportedToken() only by owner", async function () {
+    const [owner, v1] = await ethers.getSigners();
+
+    const chainId = 123;
+    const addressToken = "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF";
+    const destinationToken = "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF";
+
+    const TokenManager = await ethers.getContractFactory("TokenManager");
+    const tokenManager = await TokenManager.deploy();
+    await tokenManager.deployed();
+    
+    await (await tokenManager.initialize(v1.address)).wait();
+    await expect(tokenManager.addSupportedToken(chainId, addressToken, destinationToken)).to.be
+    .revertedWith("Ownable: caller is not the owner");
+
+  });
+  
+  it("Should adds token addresses to supportedTokens mapping", async function () {
+    const [owner] = await ethers.getSigners();
+    const ownerAddress = owner.address;
+
+    const chainId = 123;
+    const addressToken = "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF";
+    const destinationToken = "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF";
+
+    const TokenManager = await ethers.getContractFactory("TokenManager");
+    const tokenManager = await TokenManager.deploy();
+    await tokenManager.deployed();
+    
+    await (await tokenManager.initialize(ownerAddress)).wait();
+    await expect(tokenManager.addSupportedToken(chainId, addressToken, destinationToken));
+
     expect(await tokenManager.supportedTokens(chainId, addressToken)).to.equal(destinationToken);
   });
 });
+
