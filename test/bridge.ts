bb30e903fb8feb774bb95267e402447e39077706
Serhii Varakuta <flood.code@gmail.com>
2022-04-07 18:04:13 +0300

    Change tests file structure

--- test/bridge.ts
+++ test/bridge.ts
@@ -174,39 +174,3 @@ describe('Bridge', function () {
     );
   });
 });
-
-describe('TokenManager', function () {
-  it('Should called addSupportedToken() only by owner', async function () {
-    const [, v1] = await ethers.getSigners();
-
-    const chainId = 123;
-    const tokenManagerAddress = '0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF';
-    const destinationToken = '0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF';
-
-    const TokenManager = await ethers.getContractFactory('TokenManager');
-    const tokenManager = await TokenManager.deploy();
-    await tokenManager.deployed();
-    await tokenManager.initialize(v1.address);
-
-    await expect(tokenManager.addSupportedToken(chainId, tokenManagerAddress, destinationToken)).to.be.revertedWith(
-      'Ownable: caller is not the owner'
-    );
-  });
-
-  it('Should adds token addresses to supportedTokens mapping', async function () {
-    const [owner] = await ethers.getSigners();
-    const ownerAddress = owner.address;
-
-    const chainId = 123;
-    const tokenManagerAddress = '0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF';
-    const destinationToken = '0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF';
-
-    const TokenManager = await ethers.getContractFactory('TokenManager');
-    const tokenManager = await TokenManager.deploy();
-    await tokenManager.deployed();
-    await tokenManager.initialize(ownerAddress);
-
-    await tokenManager.addSupportedToken(chainId, tokenManagerAddress, destinationToken);
-    expect(await tokenManager.supportedTokens(chainId, tokenManagerAddress)).to.equal(destinationToken);
-  });
-});
