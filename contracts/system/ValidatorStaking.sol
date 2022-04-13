98ebaa4b4fc4c4fd9be21a7036b8b2c9b1c8f903
Serhii Makohon <serhii.makohon@neatfusion.org>
2022-04-14 00:35:43 +0300

    BG-13/ Implement ValidatorInfo structure

--- contracts/system/ValidatorStaking.sol
+++ contracts/system/ValidatorStaking.sol
@@ -5,6 +5,12 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ValidatorStaking is Ownable, Initializable {
+    enum ValidatorStatus{ INACTIVE, ACTIVE, SLASHED, WITHDRAWINGSMALL }
+    struct ValidatorInfo{
+        address validator;
+        uint256 stake;
+        ValidatorStatus status;
+    }
     uint256 public minimalStake;
     mapping(address => uint256) public stakes;
 
