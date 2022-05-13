34e7c8022d001937b6218611f5ffe00057f78f46
Serhii Makohon <serhii.makohon@neatfusion.org>
2022-05-13 09:28:21 +0300

    BG-54/ moved test from fee-manager.ts to liquidity-pools.ts

--- test/bridge/liquidity-pools.ts
+++ test/bridge/liquidity-pools.ts
@@ -241,4 +241,61 @@ describe('LiquidityPools', function () {
     await liquidityPools.removeLiquidity(mockToken.address, amountLiquidity);
     expect(await liquidityPools.providedLiquidity(mockToken.address)).to.equal(0);
   });
+
+  it('should distribute the reward equally', async function () {
+    const [owner, v1, v2, receiver] = await ethers.getSigners();
+    const initialRequiredApprovals = 2;
+    const amountTransfer = '1000000000000000000';
+    const amount = '100000000000000000';
+    const tokenFee = '10000';
+    const validatorReward = '10000';
+    const liquidityReward = '10000';
+    const foundationReward = '10000';
+    const fee = '100';
+    const sourceChainId = 5;
+    const txHash = '0x54c96e7f79d5fd653951c49783fc2fa7299f14c01a5a3a03f8bfb55eecb2751f';
+
+    const { liquidityPools, mockToken, bridge, feeManager } = await deployBridge(
+      owner.address,
+      [v1.address, v2.address],
+      initialRequiredApprovals
+    );
+
+    await mockToken.transfer(v1.address, amountTransfer);
+    await mockToken.transfer(v2.address, amountTransfer);
+
+    const mockToken1 = await ethers.getContractAt('MockToken', mockToken.address, v1);
+    const mockToken2 = await ethers.getContractAt('MockToken', mockToken.address, v2);
+
+    const bridge1 = await ethers.getContractAt('Bridge', bridge.address, v1);
+    const bridge2 = await ethers.getContractAt('Bridge', bridge.address, v2);
+
+    const liquidityPools1 = await ethers.getContractAt('LiquidityPools', liquidityPools.address, v1);
+    const liquidityPools2 = await ethers.getContractAt('LiquidityPools', liquidityPools.address, v2);
+
+    await mockToken1.approve(liquidityPools1.address, amount);
+    await mockToken2.approve(liquidityPools2.address, amount);
+
+    await mockToken1.approve(bridge1.address, amount);
+    await mockToken2.approve(bridge2.address, amount);
+
+    await liquidityPools1.addLiquidity(mockToken1.address, amount);
+    await liquidityPools2.addLiquidity(mockToken2.address, amount);
+
+    await bridge1.deposit(mockToken1.address, sourceChainId, receiver.address, amount);
+
+    await bridge1.approveTransfer(txHash, mockToken.address, sourceChainId, receiver.address, amount);
+    await bridge2.approveTransfer(txHash, mockToken.address, sourceChainId, receiver.address, amount);
+
+    await feeManager.setTokenFee(mockToken.address, tokenFee, validatorReward, liquidityReward, foundationReward);
+    await feeManager.distributeRewards(mockToken.address);
+
+    expect(await liquidityPools.collectedFees(mockToken.address)).to.equal(fee);
+
+    await liquidityPools1.claimRewards(mockToken1.address);
+    expect(await liquidityPools.collectedFees(mockToken.address)).to.equal(Number(fee) / 2);
+
+    await liquidityPools2.claimRewards(mockToken2.address);
+    expect(await liquidityPools.collectedFees(mockToken.address)).to.equal(0);
+  });
 });
