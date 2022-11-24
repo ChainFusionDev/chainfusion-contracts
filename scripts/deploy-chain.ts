d9cafee4255d9ba254abd02d7b4c063231d1738d
serhii.makohon <serhii.makohon@chainfusion.org>
2022-11-24 13:09:57 +0200

    BG-492 generate bridgeconfig.yaml file

--- scripts/deploy-chain.ts
+++ scripts/deploy-chain.ts
@@ -1,10 +1,11 @@
-import { network } from 'hardhat';
+import { ethers, network } from 'hardhat';
 import { deployBridgeContracts } from './deploy/chain';
-import { readChainContractsConfig, readContractsConfig, updateContractsConfig, writeChainContractsConfig } from './deploy/config';
+import { readChainContractsConfig, readContractsConfig, updateContractsConfig, writeChainContractsConfig, processBridgeConfig } from './deploy/config';
 
 async function main() {
   const verify = (process.env.VERIFY || '').trim().toLowerCase() === 'true';
   const chainId = network.config.chainId ?? 1;
+  const blockNumber = await ethers.provider.getBlockNumber();
 
   const homeContractsConfig = await readContractsConfig();
   const homeNetwork = homeContractsConfig.networkName;
@@ -21,6 +22,8 @@ async function main() {
 
   updateContractsConfig(contractsConfig, res);
   await writeChainContractsConfig(chainId, contractsConfig);
+
+  await processBridgeConfig(res.relayBridge.address, res.signerStorage.address, blockNumber)
 }
 
 main().catch((error) => {
--- scripts/deploy/config.ts
+++ scripts/deploy/config.ts
@@ -1,5 +1,6 @@
 import { BaseContract } from 'ethers';
 import { promises as fs } from 'fs';
+import { parse, stringify } from 'yaml';
 
 export interface ContractsConfig {
   [key: string]: string | undefined;
@@ -46,6 +47,73 @@ export async function readChainContractsConfig(chainId: number): Promise<Contrac
 export async function writeChainContractsConfig(chainId: number, config: ContractsConfig): Promise<void> {
   try {
     await fs.writeFile(`contracts-${chainId}.json`, JSON.stringify(config, null, 2));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export interface BridgeConfig {
+  [key: string]: Config | undefined;
+}
+
+export interface Config {
+  chainId: Number;
+  rpcURL: string;
+  bridgeContract: string;
+  signerStorageContract: string;
+  startBlock: Number;
+}
+
+export async function processBridgeConfig(
+  bridgeContract: string,
+  signerStorageContract: string,
+  startBlock: Number
+): Promise<void> {
+  const bridgeConfig = await readBridgeConfig();
+
+  updateBridgeConfig(bridgeConfig, bridgeContract, signerStorageContract, startBlock);
+  await writeBridgeConfig(bridgeConfig);
+}
+
+export async function updateBridgeConfig(
+  bridgeConfig: BridgeConfig,
+  bridgeContract: string,
+  signerStorageContract: string,
+  startBlock: Number
+): Promise<BridgeConfig> {
+  const hardhat = require('hardhat');
+
+  const rpcURL = hardhat.network.config.url;
+  const chainId = hardhat.network.config.chainId;
+  const name = hardhat.network.name;
+
+  var con: Config = {
+    chainId: chainId,
+    rpcURL: rpcURL,
+    bridgeContract: bridgeContract,
+    signerStorageContract: signerStorageContract,
+    startBlock: startBlock,
+  };
+
+  bridgeConfig[name] = con;
+
+  return bridgeConfig;
+}
+
+export async function readBridgeConfig(): Promise<BridgeConfig> {
+  try {
+    const fileData = await fs.readFile('bridgeconfig.yaml');
+    const config = parse(fileData.toString());
+
+    return config;
+  } catch (error) {
+    return {};
+  }
+}
+
+export async function writeBridgeConfig(bg: BridgeConfig): Promise<void> {
+  try {
+    await fs.writeFile('bridgeconfig.yaml', stringify(bg, null, 2));
   } catch (error) {
     console.error(error);
   }
